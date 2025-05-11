eb9dcbe2d0bce560adc5eb4829718545164a22e3
Brandon Pershall <brandonj.pershall@gmail.com>
2025-05-11 15:20:45 -0600

    Update bullshit shity shit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import {makeHttpRequest} from './makeRequest.ts'
 
 defineProps<{ msg: string }>()
 
@@ -11,7 +10,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="makeHttpRequest">count is {{ count }}</button>
+    <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
