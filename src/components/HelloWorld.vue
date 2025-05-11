23d20f1aed2f1b22a3b8197544ab3213309ce49e
Brandon Pershall <brandonj.pershall@gmail.com>
2025-05-11 15:24:44 -0600

    Update bullshit shity shit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="makeHttpRequest('POST', 'test')">count is {{ count }}</button>
+    <button type="button" @click="makeHttpRequest('google.com','GEt')">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
