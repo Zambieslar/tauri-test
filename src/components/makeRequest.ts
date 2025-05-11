91d95f9c7c9ec5faa5e8866277789c9daed27df5
Brandon Pershall <brandonj.pershall@gmail.com>
2025-05-11 15:15:56 -0600

    Test http commit

---
+++ src/components/makeRequest.ts
@@ -0,0 +1,19 @@
+export async function makeHttpRequest(url: string, method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET', body: any = null, headers: HeadersInit = {}) {
+    try {
+        const response = await fetch(url, {
+            method,
+            headers,
+            body: body ? JSON.stringify(body) : null,
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("There was an error sending the request:", error);
+        throw error;
+    }
+}
